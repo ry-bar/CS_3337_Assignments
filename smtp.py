b8070219bf43cc2a5e77974a22c352a37beddc75
Ryan Bartel <104879351+ry-bar@users.noreply.github.com>
2023-09-24 16:13:26 -0600

    Update smtp.py
    Finished the script. Should be good to turn in.

--- smtp.py
+++ smtp.py
@@ -1,7 +1,23 @@
 from socket import *
 
+
 # This is not a well-formed email message (see RFC2822)
-msg = "\r\n I love computer networks!"
+msg = ("From: futurefriend@gmail.com\r\n"
+       "To: wrigjaso@isu.edu\r\n"
+       "Subject: Urgent!\r\n"
+       "Date: Sun, 16 February 2020 15:20:00 -0700\r\n"
+       "\r\n"# An empty line to separate the header from the body.
+       "I have urgent information from the future for you. I've recently learned how dependable and reliable SMTP "
+       "protocol is. I have no doubt, unless for some strange reason this email is carelessly deleted immediately "
+       "upon receipt, that you will be able to act upon the information you are about to receive. In just a little "
+       "over one month toilet paper will become scarce and nearly unobtainable. Please, heed my warning and stock up "
+       "now, before it is too late. There is no point in both of us being caught off guard in these dark times."
+       "\n"
+       "With care,\n"
+       "You're friend from the future"
+       "\n"
+       "\n"
+       "P.S. Dogecoin peaks at $0.68\r\n")
 
 # This -is- the correct way to end a message after the data command
 endmsg = "\r\n.\r\n"
@@ -34,10 +50,6 @@ if recv1[:3] != '250':
 
 
 
-
-
-
-
 # Send MAIL FROM command and print server response.
 # Fill in start
 myEmail = "bartrya2@isu.edu"# Made this a variable to make it easier to change the MAIL FROM, just in case.
@@ -51,9 +63,6 @@ print(f"mailFromCommand:\n\t{recv2}")
 if recv2[:3] != '250':
     print('250 reply not received from server.')
 # Fill in end
-
-
-
 
 
 # Send RCPT TO command and print server response.
@@ -71,48 +80,45 @@ if recv3[:3] != '250':
 # Fill in end
 
 
-
-
-
-
 # Send DATA command and print server response.
 # Fill in start
 dataCommand = "DATA\r\n"
-clientSocket.send(dataCommand.encode())
+clientSocket.send(dataCommand.encode())# This is what will be sent to the server.
 
-
+# Following the same structure from the code given above from Professor Wright
 recv4 = clientSocket.recv(1024).decode()
+print(f"dataCommand:\n\t{recv4}")
 if recv4[:3] != '354':
-    print('354 response not received from server')
+    print('354 response not received from server.k')
 
 # Fill in end
-
-
-
-
 
 
 # Send message data.
 # Fill in start
-
+clientSocket.send(msg.encode())# Sending the email
 # Fill in end
-
-
-
-
-
 
 # Message ends with a single period.
 # Fill in start
+clientSocket.send(endmsg.encode())# Ending the email
 
+recv5 = clientSocket.recv(1024).decode()
+print(f"endmsg:\n\t{recv5}")
+if recv5[:3] != '250':
+    print('250 response not received from server.')
 # Fill in end
-
-
-
-
 
 
 # Send QUIT command and get server response.
 # Fill in start
-clientSocket.close()
+quitCommand = "QUIT\r\n"
+clientSocket.send(quitCommand.encode())# This is what will be sent to the server.
+
+# Following the same structure from the code given above from Professor Wright
+recv6 = clientSocket.recv(1024).decode()
+print(f"quitCommand:\n\t{recv6}")
+if recv6[:3] != '221':
+    print("221 response not received from the server.")
+
 # Fill in end
